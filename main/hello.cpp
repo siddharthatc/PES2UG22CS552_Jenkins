e18c0b9d098b8700c399aa717abec5e49e207b7f
Siddhartha <tcsiddhartha@gmail.com>
2025-03-13 13:38:05 +0530

    Task 4 successful

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout<< " Hello, PES2UG22CS552\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
